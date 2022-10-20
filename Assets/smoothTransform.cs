4ce6ca96de5226599a59d30cdc8ed40b5f966826
Janek <j.brunow@lastlevel.pl>
2022-10-20 17:01:19 +0200

    SmoothDamp

---
+++ Assets/smoothTransform.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoothTransform : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    float smoothTime = 2f;
+    float yVelocity = 0.0f;
+
+    void Update()
+    {
+        float newPosition = Mathf.SmoothDamp(transform.position.y, target.position.y, ref yVelocity, smoothTime);
+        transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
+    }
+}
