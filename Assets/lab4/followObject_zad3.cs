cc780e2199646276ef5b4ab46ddf257ba8244c62
Janek <j.brunow@lastlevel.pl>
2022-10-30 11:34:45 +0100

    lab4_zad3

---
+++ Assets/lab4/followObject_zad3.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followObject_zad3 : MonoBehaviour
+{
+    [SerializeField] private Transform followTransform;
+    private Vector3 offset;
+    
+    void Start()
+    {
+        offset = transform.position - followTransform.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 nextPosition = followTransform.position + offset;
+        this.transform.position = nextPosition;
+    }
+}
